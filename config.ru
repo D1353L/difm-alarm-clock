41ada97898b72c4a0ee4455830bf8dd44a86f0cc
D1353L <meghanik400@gmail.com>
2016-11-15 20:46:00 +0200

    Application chenged to the static type without any server. Ability to choose radiostation is added

--- config.ru
+++
@@ -1,5 +0,0 @@
-require_relative 'public/racker.rb'
-require 'bundler'
-Bundler.require
-use Rack::Static, urls: ["/public"]
-run Racker.new
---
+++ scripts.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+
+    LoadStationsDropdown('stations.json');
+
+    setInterval(function(){
+        $('#time_now').text(new Date());
+    }, 500);
+
+    $('#datetimepicker').datetimepicker({
+        inline: true,
+        sideBySide: true,
+        format: "DD/MM/YYYY HH:mm",
+        minDate: new Date()
+    });
+    window.refreshIntervalId = null;
+
+    $(document).on('click', '#set_wakeup_dt', function () {
+        var wakeup_dt = $('#datetimepicker').data("DateTimePicker").date();
+        wakeup_dt.seconds(0);
+
+        if(window.refreshIntervalId) clearInterval(window.refreshIntervalId);
+
+        if(wakeup_dt > moment())
+        {
+            var timeout = wakeup_dt - moment(),
+                duration = moment.duration(timeout, 'milliseconds'),
+                interval = 1000
+                success = false;
+
+            window.refreshIntervalId = setInterval(function(){
+                duration = moment.duration(duration.asMilliseconds() - interval, 'milliseconds');
+
+                if(duration > moment.duration(0))
+                {
+                    msg =  "Time to wakeup: " + duration.hours() + ":" + duration.minutes() + ":" + duration.seconds();
+                }
+                else
+                {
+                    msg =  "WAKEUP!";
+                    var win = window.open($('#stations > select').val(), '_blank');
+                    if(win) win.focus();
+                    success = true;
+                }
+
+                $('#time_to_wakeup').text(msg);
+                if(success) clearInterval(window.refreshIntervalId);
+            }, interval);
+        }
+        else {alert("Wakeup datetime should be in the future");}
+    });
+
+});
+
+function LoadStationsDropdown(filePath)
+{
+    $.getJSON(filePath, function(data) {
+        var select = document.createElement("select");
+        data.forEach(function(item){
+            select.options.add( new Option(item.name, item.url));
+        });
+        $('#stations').append(select);
+    });
+}
